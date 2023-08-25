197e71ed5e745f94b21787b9ed41f59380e2e16d
rohit <78015007+rohitjoshi1231@users.noreply.github.com>
2023-08-25 21:01:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,19 +10,23 @@ def root():
 
 
 @app.route("/submit_form", methods=['POST', 'GET'])
-def submit_form(value=None, val = None):
-    if request.method == "POST":
-        data = request.form.to_dict()
-        link = data["vid_link"]
-        yt = YouTube(link, on_progress_callback=on_progress)
-        views = yt.views
-        title = yt.title
-        thumbnail = yt.thumbnail_url
-        d_video = yt.streams.first()
-        x = title+"\n "+"Total views"+" "+str(views)
-        d_video.download('.\\downloads\\')
-
-    return render_template("./main.html", Title=x, T =thumbnail)
+def submit_form(value=None, val=None):
+    try:
+        if request.method == "POST":
+            data = request.form.to_dict()
+            link = data["vid_link"]
+            yt = YouTube(link, on_progress_callback=on_progress)
+            views = yt.views if yt.views else 0
+            title = yt.title
+            thumbnail = yt.thumbnail_url
+            print(title)
+            d_video = yt.streams.get_highest_resolution()
+            x = title+"\n "+"Total views"+" ", views
+            d_video.download(r'C:\Users\DELL\downloads')
+            return render_template("main.html", title=title, views=views, thumbnail=thumbnail)
+    except Exception as e:
+        error_message = str(e)
+        return render_template("main.html", error_message=error_message)
 
 
 if __name__ == '__main__':
